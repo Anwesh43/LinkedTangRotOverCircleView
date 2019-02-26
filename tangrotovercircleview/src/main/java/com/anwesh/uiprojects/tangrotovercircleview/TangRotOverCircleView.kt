92da9d9c630a6bc02eebf706e26e30a86238810c
Anwesh43 <anweshthecool0@gmail.com>
2019-02-27 02:16:26 +0530

    adding drawTROCNode canvas extension method

--- tangrotovercircleview/src/main/java/com/anwesh/uiprojects/tangrotovercircleview/TangRotOverCircleView.kt
+++ tangrotovercircleview/src/main/java/com/anwesh/uiprojects/tangrotovercircleview/TangRotOverCircleView.kt
@@ -27,3 +27,28 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
+fun Int.sf() : Float = 1f - 2 * (this % 2)
+
+fun Canvas.drawTROCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    drawCircle(0f, 0f, size, paint)
+    for (j in 0..(lines - 1)) {
+        save()
+        rotate(90f * j.sf() * sc1.divideScale(j, lines))
+        drawLine(-size / 2, -size, size / 2, -size, paint)
+        restore()
+    }
+    restore()
+}
