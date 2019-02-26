7299af7f7402bbccabc7398b178eafff033aa496
Anwesh43 <anweshthecool0@gmail.com>
2019-02-27 02:27:37 +0530

    adding TROCNode class

--- tangrotovercircleview/src/main/java/com/anwesh/uiprojects/tangrotovercircleview/TangRotOverCircleView.kt
+++ tangrotovercircleview/src/main/java/com/anwesh/uiprojects/tangrotovercircleview/TangRotOverCircleView.kt
@@ -117,4 +117,46 @@ class TangRotOverCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TROCNode(var i : Int, val state : State = State()) {
+        private var next : TROCNode? = null
+        private var prev : TROCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TROCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTROCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TROCNode {
+            var curr : TROCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
