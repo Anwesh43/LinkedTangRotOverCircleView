ebfbfff2a1985c46b762348d48bb3f13d75238e8
Anwesh43 <anweshthecool0@gmail.com>
2019-02-27 09:21:30 +0530

    adding delay as a constant

--- tangrotovercircleview/src/main/java/com/anwesh/uiprojects/tangrotovercircleview/TangRotOverCircleView.kt
+++ tangrotovercircleview/src/main/java/com/anwesh/uiprojects/tangrotovercircleview/TangRotOverCircleView.kt
@@ -20,6 +20,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#212121")
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -97,7 +98,7 @@ class TangRotOverCircleView(ctx : Context) : View(ctx) {
             if (animated) {
                 cb()
                 try {
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                     view.invalidate()
                 } catch(ex : Exception) {
 
